bee88b9ae4db9f06fd95710153b8aabc20350100
Guilherme Fabrin Franco <guilherme.fabrin@gmail.com>
2019-03-20 11:21:41 -0300

    Adição de controle de autenticação do Laravel

---
+++ resources/lang/pt-br/auth.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'As informações de login não foram encontradas.',
+    'throttle' => 'Muitas tentativas de login. Por favor tente novamente em :seconds segundos.',
+    'login' => [
+        'title' => 'Entrar',
+        'email' => 'Endereço de email',
+        'password' => 'Senha',
+        'remember_me' => 'Lembrar me',
+        'forgot_password' => 'Esqueci minha senha',
+        'login' => 'Entrar'
+    ],
+    'register' => [
+        'title' => 'Registrar',
+        'name' => 'Nome',
+        'email' => 'Endereço de email',
+        'password' => 'Senha',
+        'confirm_password' => 'Confirmar senha',
+        'register' => 'Criar minha conta'
+    ],
+    'email' => [
+        'title' => 'Resetar senha',
+        'email' => 'Endereço de email',
+        'send' => 'Enviar email'
+    ],
+    'reset'=>[
+        'title' => 'Resetar senha',
+        'email' => 'Endereço de email',
+        'password' => 'Senha',
+        'confirm_password' => 'Confirmar senha',
+        'reset' => 'Resetar senha'
+    ]
+];
---
+++ resources/lang/pt-br/layouts.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+	'app' => [
+		'login' => 'Login',
+		'register' => 'Registrar',
+		'toggle_navigation' => 'Esconder navegação'
+	]
+];
