843a0ba11c4bb067ba7824f6dd5131654e178c2f
Guilherme Fabrin Franco <guilherme.fabrin@gmail.com>
2019-03-20 13:11:15 -0300

    Config migration deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,24 +18,6 @@ set :linked_files, %w{.env}
 set :keep_releases, 2
 
 namespace :deploy do
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
+  invoke 'laravel:migrate'
   after :finishing, 'deploy:cleanup'
-
 end
