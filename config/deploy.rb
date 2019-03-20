767bf8b843d8eccdc480ca428ffac84329d16c67
Guilherme Fabrin Franco <guilherme.fabrin@gmail.com>
2019-03-20 10:28:45 -0300

    Configuração para não copiar .env para servidor.

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,13 +4,14 @@ set :repo_url, ENV['DEPLOY_REPO']
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 set :deploy_to, ENV['DEPLOY_TO']
+
 # set :scm, :git
 
 # set :format, :pretty
 # set :log_level, :debug
 # set :pty, true
 
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{.env}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -15,10 +15,12 @@ set :stage, :production
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
+
+set :laravel_upload_dotenv_file_on_deploy, false
 server ENV['DEPLOY_PROD_HOST'], user: ENV['DEPLOY_PROD_USER'], ssh_options: {
 	auth_methods: %w(password),
 	password: ENV['DEPLOY_PROD_PASS']
-}, laravel_upload_dotenv_file_on_deploy: false, laravel_dotenv_file: '../shared/.env'
+}
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
