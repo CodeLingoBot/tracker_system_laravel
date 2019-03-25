f8d59c3ece0832892399a27d1cf8c1df76e59d4d
Guilherme Fabrin Franco <guilherme.fabrin@gmail.com>
2019-03-25 13:42:27 -0300

    Não deixar ver usuários de usuários.

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -15,6 +15,7 @@ class UsersController extends Controller
     }
 
     public function index(User $user){
+        if (!$user->isAdmin() && !$user->isSubAdmin()) return redirect(route('users'));
         $users = User::where(['created_by' => $user->id])->paginate(Setting::paginacao());
         return view('laravelusers::usersmanagement.show-users', [
             'fromUser' =>  $user,
