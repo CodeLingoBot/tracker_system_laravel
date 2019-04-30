c51af99bd27f3732cd262b271bbf0d0807158df5
Guilherme Fabrin Franco <guilherme.fabrin@gmail.com>
2019-04-30 18:42:18 +0000

    Show hex string and continue loop

--- sockets/crx1.php
+++ sockets/crx1.php
@@ -1,6 +1,7 @@
-﻿<?php
+<?php
 include 'autoload.php';
 $config = include 'config/crx1.php';
 Socket::loop($config['ip'], $config['port'], function ($buffer) {
-	log_info("application", $buffer);
+    $hexString = bin2hex(join(array_map("chr", $buffer)));
+	log_info("application", $hexString);
 });
