54f3896916ed99153266b010fce74b7bfdf4d3ea
Guilherme Fabrin Franco <guilherme.fabrin@gmail.com>
2019-04-30 20:28:55 +0000

    Configuração com banco de dados

--- sockets/crx1.php
+++ sockets/crx1.php
@@ -3,7 +3,7 @@ include 'autoload.php';
 $config = include 'config/crx1.php';
 Socket::loop($config['ip'], $config['port'], function ($buffer) {
     if (empty($buffer)) return;
-    $connection = new mysqli($servername, $username, $password, $dbname);
+    $connection = new mysqli(getenv('DB_HOST'), getenv('DB_USERNAME'), getenv('DB_PASSWORD'), getenv('DB_DATABASE'));
     if ($connection->connect_error) {
         log_info("app_crx1", "Connection failed: " . $connection->connect_error);
         return;
