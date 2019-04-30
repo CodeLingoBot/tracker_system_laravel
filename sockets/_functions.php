f613b6f226be7cd980fec7b5648fa5b90d7c3ee1
Guilherme Fabrin Franco <guilherme.fabrin@gmail.com>
2019-04-30 19:17:40 +0000

    teste

--- sockets/_functions.php
+++ sockets/_functions.php
@@ -1,1 +1,26 @@
 <?php
+function buffer2hex($data)
+{
+	static $from = '';
+	static $to = '';
+	static $width = 50;
+	static $pad = '.';
+	if ($from==='')
+	{
+		for ($i=0; $i<=0xFF; $i++)
+		{
+			$from .= chr($i);
+			$to .= ($i >= 0x20 && $i <= 0x7E) ? chr($i) : $pad;
+		}
+	}
+	$hex = str_split(bin2hex($data), $width * 2);
+	$chars = str_split(strtr($data, $from, $to), $width);
+	$offset = 0;
+	$retorno = '';
+	foreach ($hex as $i => $line)
+	{
+		$retorno .= implode(' ', str_split($line, 2));
+		$offset += $width;
+	}
+	return $retorno;
+}
--- sockets/crx1.php
+++ sockets/crx1.php
@@ -2,6 +2,5 @@
 include 'autoload.php';
 $config = include 'config/crx1.php';
 Socket::loop($config['ip'], $config['port'], function ($buffer) {
-  $chars = array_map("chr", $buffer);
-  var_dump(join($chars));
+  lof_info(buffer2hex($buffer.""));
 });
