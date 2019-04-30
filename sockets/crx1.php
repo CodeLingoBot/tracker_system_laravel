4a7b994fb36d130d93f044382c3c101c5a695e9b
Guilherme Fabrin Franco <guilherme.fabrin@gmail.com>
2019-04-30 19:01:41 +0000

    Add funcions

--- sockets/crx1.php
+++ sockets/crx1.php
@@ -2,5 +2,6 @@
 include 'autoload.php';
 $config = include 'config/crx1.php';
 Socket::loop($config['ip'], $config['port'], function ($buffer) {
-    hex_dump(bin2hex(join(array_map("chr", $buffer))));
+  $chars = array_map("chr", $buffer);
+  var_dump(join($chars));
 });
