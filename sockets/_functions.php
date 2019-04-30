a45bde94a0a8f0cd65236c1259e8da8fda637d35
Guilherme Fabrin Franco <guilherme.fabrin@gmail.com>
2019-04-30 19:29:44 +0000

    teste

--- sockets/_functions.php
+++ sockets/_functions.php
@@ -3,31 +3,25 @@ function buffer2hex($data)
 {
 	static $from = '';
 	static $to = '';
-
-	static $width = 16; # number of bytes per line
-
+	static $width = 50; # number of bytes per line
 	static $pad = '.'; # padding for non-visible characters
-
 	if ($from==='')
 	{
 		for ($i=0; $i<=0xFF; $i++)
 		{
-		$from .= chr($i);
-		$to .= ($i >= 0x20 && $i <= 0x7E) ? chr($i) : $pad;
+			$from .= chr($i);
+			$to .= ($i >= 0x20 && $i <= 0x7E) ? chr($i) : $pad;
 		}
 	}
-
 	$hex = str_split(bin2hex($data), $width*2);
 	$chars = str_split(strtr($data, $from, $to), $width);
-
 	$offset = 0;
-	$retorno = "";
+	$retorno = '';
 	foreach ($hex as $i => $line)
 	{
-		$implosion = implode(' ', str_split($line,2));
-		$retorno .= $implosion;
-		log_info("app_crx1", sprintf('%6X', $offset).' : ' . $implosion . ' [' . $chars[$i] . ']');
+		$retorno .= implode(' ', str_split($line,2));
 		$offset += $width;
 	}
 	return $retorno;
+	sprintf($retorno);
 }
