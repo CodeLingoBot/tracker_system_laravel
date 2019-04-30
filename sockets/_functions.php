b6698b41d5313bdf37f4f7ae1f1155c156e1c592
Guilherme Fabrin Franco <guilherme.fabrin@gmail.com>
2019-04-30 19:24:39 +0000

    teste

--- sockets/_functions.php
+++ sockets/_functions.php
@@ -3,24 +3,30 @@ function buffer2hex($data)
 {
 	static $from = '';
 	static $to = '';
-	static $width = 50;
-	static $pad = '.';
+
+	static $width = 16; # number of bytes per line
+
+	static $pad = '.'; # padding for non-visible characters
+
 	if ($from==='')
 	{
 		for ($i=0; $i<=0xFF; $i++)
 		{
-			$from .= chr($i);
-			$to .= ($i >= 0x20 && $i <= 0x7E) ? chr($i) : $pad;
+		$from .= chr($i);
+		$to .= ($i >= 0x20 && $i <= 0x7E) ? chr($i) : $pad;
 		}
 	}
-	$hex = str_split(bin2hex($data), $width * 2);
+
+	$hex = str_split(bin2hex($data), $width*2);
 	$chars = str_split(strtr($data, $from, $to), $width);
+
 	$offset = 0;
-	$retorno = '';
 	foreach ($hex as $i => $line)
 	{
-		$retorno .= implode(' ', str_split($line, 2));
+		$implosion = implode(' ', str_split($line,2));
+		$retorno .= $implosion;
+		log_info("app_crx1", sprintf('%6X', $offset).' : ' . $implosion . ' [' . $chars[$i] . ']');
 		$offset += $width;
 	}
-	return $retorno;
+	return $implosion;
 }
--- sockets/crx1.php
+++ sockets/crx1.php
@@ -2,5 +2,5 @@
 include 'autoload.php';
 $config = include 'config/crx1.php';
 Socket::loop($config['ip'], $config['port'], function ($buffer) {
-  log_info("app_crx1", buffer2hex($buffer.""));
+  $hex = buffer2hex($buffer."");
 });
