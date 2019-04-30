b0af7301901431bd70eaab1da5f0bbfa3b8e2f71
Guilherme Fabrin Franco <guilherme.fabrin@gmail.com>
2019-04-30 18:46:20 +0000

    Show hex string and continue loop

--- sockets/_functions.php
+++ sockets/_functions.php
@@ -1,30 +1,55 @@
 <?php
 
 function envia_comando($imei){
-    $cnx = mysql_connect("cloudservice.cgejdsdl842e.sa-east-1.rds.amazonaws.com", "gpstracker", "d1$1793689")
-    or die("Could not connect: " . mysql_error());
-    mysql_select_db('tracker', $cnx);
-    $res = mysql_query("SELECT c.command FROM command c WHERE c.imei = '$imei' ORDER BY date DESC LIMIT 1");
-    while ($data = mysql_fetch_assoc($res)) {
-        $send_cmd = $data['command'];
-        socket_send($socket, $send_cmd, strlen($send_cmd), 0);
-    }
-    mysql_query("DELETE FROM command WHERE imei = $imei");
-    mysql_query("insert into teste(id,string) values(null, '$send_cmd')", $cnx);
-    mysql_close($cnx);
-    printLog($fh, "Comandos do arquivo apagado: " . $send_cmd . " imei: " . $imei);
+      $cnx = mysql_connect("cloudservice.cgejdsdl842e.sa-east-1.rds.amazonaws.com", "gpstracker", "d1$1793689")
+      or die("Could not connect: " . mysql_error());
+      mysql_select_db('tracker', $cnx);
+      $res = mysql_query("SELECT c.command FROM command c WHERE c.imei = '$imei' ORDER BY date DESC LIMIT 1");
+      while ($data = mysql_fetch_assoc($res)) {
+            $send_cmd = $data['command'];
+            socket_send($socket, $send_cmd, strlen($send_cmd), 0);
+      }
+      mysql_query("DELETE FROM command WHERE imei = $imei");
+      mysql_query("insert into teste(id,string) values(null, '$send_cmd')", $cnx);
+      mysql_close($cnx);
+      printLog($fh, "Comandos do arquivo apagado: " . $send_cmd . " imei: " . $imei);
 }
 
 function gprsToGps($cord, $hemisphere)
 {
-    $novaCord = 0;
-    strlen($cord) == 9 && $cord = '0' . $cord;
-    $g = substr($cord, 0, 3);
-    $d = substr($cord, 3);
-    $novaCord = $g + ($d / 60);
-    if ($hemisphere == "S")
-        $novaCord = $novaCord * -1;
-    if ($hemisphere == "W")
-        $novaCord = $novaCord * -1;
-    return $novaCord;
+      $novaCord = 0;
+      strlen($cord) == 9 && $cord = '0' . $cord;
+      $g = substr($cord, 0, 3);
+      $d = substr($cord, 3);
+      $novaCord = $g + ($d / 60);
+      if ($hemisphere == "S")
+            $novaCord = $novaCord * -1;
+      if ($hemisphere == "W")
+            $novaCord = $novaCord * -1;
+      return $novaCord;
+}
+
+function hex_dump($data, $newline="\n")
+{
+    static $from = '';
+    static $to = '';
+    static $width = 16; # number of bytes per line
+    static $pad = '.'; # padding for non-visible characters
+    if ($from==='')
+    {
+      for ($i=0; $i<=0xFF; $i++)
+      {
+          $from .= chr($i);
+          $to .= ($i >= 0x20 && $i <= 0x7E) ? chr($i) : $pad;
+      }
+    }
+    $hex = str_split(bin2hex($data), $width*2);
+    $chars = str_split(strtr($data, $from, $to), $width);
+
+    $offset = 0;
+    foreach ($hex as $i => $line)
+    {
+      lof_info(sprintf('%6X',$offset).' : '.implode(' ', str_split($line,2)) . ' [' . $chars[$i] . ']');
+      $offset += $width;
+    }
 }
--- sockets/crx1.php
+++ sockets/crx1.php
@@ -2,6 +2,5 @@
 include 'autoload.php';
 $config = include 'config/crx1.php';
 Socket::loop($config['ip'], $config['port'], function ($buffer) {
-    $hexString = bin2hex(join(array_map("chr", $buffer)));
-	log_info("application", $hexString);
+    hex_dump($buffer);
 });
