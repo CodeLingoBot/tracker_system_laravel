00eeb32162511faf2c5a9e866852408c5af963fd
Guilherme Fabrin Franco <guilherme.fabrin@gmail.com>
2019-05-01 11:27:17 +0000

    send command

--- sockets/autoload.php
+++ sockets/autoload.php
@@ -2,6 +2,7 @@
 include '_functions.php';
 include __DIR__.'/../vendor/autoload.php';
 include 'config/_general.php';
+include 'classes/CRX.class.php';
 include 'classes/Socket.class.php';
 $dotenv = Dotenv\Dotenv::create(__DIR__."/../");
 $dotenv->load();
