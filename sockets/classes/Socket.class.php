a3199bfc8d41a2d19f61b2db2859cd824e023205
Guilherme Fabrin Franco <guilherme.fabrin@gmail.com>
2019-05-01 11:27:04 +0000

    send command

--- sockets/classes/Socket.class.php
+++ sockets/classes/Socket.class.php
@@ -5,33 +5,33 @@ class Socket{
         while(true){
             try
             {
-                if (($sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)) === false) {
+                if (($socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)) === false) {
                     log_info("app_crx1","socket_create() failed: reason: " . socket_strerror(socket_last_error()));
                     continue;
                 }
 
-                if (socket_bind($sock, $address, $port) === false) {
-                    log_info("app_crx1","socket_bind() failed: reason: " . socket_strerror(socket_last_error($sock)));
+                if (socket_bind($socket, $address, $port) === false) {
+                    log_info("app_crx1","socket_bind() failed: reason: " . socket_strerror(socket_last_error($socket)));
                     continue;
                 }
 
-                if (socket_listen($sock, 5) === false) {
-                    log_info("app_crx1","socket_listen() failed: reason: " . socket_strerror(socket_last_error($sock)));
+                if (socket_listen($socket, 5) === false) {
+                    log_info("app_crx1","socket_listen() failed: reason: " . socket_strerror(socket_last_error($socket)));
                     continue;
                 }
 
                 do {
                     log_info("app_crx1","waiting");
-                    if (($msgSock = socket_accept($sock)) === false) {
-                        log_info("app_crx1","socket_accept() failed: reason: " . socket_strerror(socket_last_error($sock)));
+                    if (($msgSock = socket_accept($socket)) === false) {
+                        log_info("app_crx1","socket_accept() failed: reason: " . socket_strerror(socket_last_error($socket)));
                         break;
                     }
                     while(socket_recv($msgSock, $buffer, 2048, 0x40) !== 0) {
-                        $function($buffer);
+                        $function($buffer, $socket);
                     }
                     socket_close($msgSock);
                 } while (true);
-                socket_close($sock);
+                socket_close($socket);
             } catch (Exception $exception) {
                 log_info("app_crx1","Exception: ". $exception->getMessage());
             }
--- sockets/crx1.php
+++ sockets/crx1.php
@@ -1,18 +1,28 @@
 <?php
 include 'autoload.php';
 $config = include 'config/crx1.php';
-Socket::loop($config['ip'], $config['port'], function ($buffer) {
+Socket::loop($config['ip'], $config['port'], function ($buffer, $socket) {
     if (empty($buffer)) return;
-    $connection = new mysqli(getenv('DB_HOST'), getenv('DB_USERNAME'), getenv('DB_PASSWORD'), getenv('DB_DATABASE'));
+    $hexString = trim(buffer2hex($buffer.""));
+    log_info("app_crx1", $hexString);
+    $hexArray = explode(' ', $hexString);
+    $hexArrayLen = count($hexArray);
+    if ($hexArrayLen < 4 || $hexArray[0].$hexArray[1] != '7878') return;
+    $packageLength = $hexArray[2];
+    $protocolNumber = $hexArray[3];
+    if ($protocolNumber=="01"){
+        $command = CRX::protocol01($hexArray);
+        log_info($command);
+        socket_send($socket, $command, strlen($command), 0);
+    }
+    /*$connection = new mysqli(getenv('DB_HOST'), getenv('DB_USERNAME'), getenv('DB_PASSWORD'), getenv('DB_DATABASE'));
     if ($connection->connect_error) {
         log_info("app_crx1", "Connection failed: " . $connection->connect_error);
         return;
     }
-    $hexString = trim(buffer2hex($buffer.""));
-    log_info("app_crx1", $hexString);
     $query = "INSERT INTO received (script, hexa) VALUES ('crx1', '".$hexString."')";
     if ($connection->query($query) !== TRUE) {
         log_info("app_crx1", "Error: " . $query . "<br>" . $connection->error);
     }
-    $connection->close();
+    $connection->close();*/
 });
