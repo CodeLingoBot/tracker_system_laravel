1fd7b273ca73c9158dceb7ba07eff1e304531345
Guilherme Fabrin Franco <guilherme.fabrin@gmail.com>
2019-05-05 17:04:53 +0000

    Implementação de composer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        \URL::forceRootUrl(\Config::get('app.url'));
+        if (str_contains(\Config::get('app.url'), 'https://')) {
+            \URL::forceScheme('https');
+        }
     }
 
     /**
--- database/migrations/2019_05_01_141355_add_imei_to_terminal_information_table.php
+++ database/migrations/2019_05_01_141355_add_imei_to_terminal_information_table.php
@@ -14,7 +14,7 @@ class AddImeiToTerminalInformationTable extends Migration
     public function up()
     {
         Schema::table('terminal_information', function (Blueprint $table) {
-            $table->string('imei');
+            $table->string('imei')->nullable();
         });
     }
 
